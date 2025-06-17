a20b39b7f3b5dcbba2a340e72520c117ea72daeb
Enzo <enzo.silveira2@gmail.com>
2025-06-17 12:47:15 -0300

    rota enviada 43:02

--- server.js
+++ server.js
@@ -9,17 +9,30 @@
 // server.listen(3333)
 
 import { fastify } from 'fastify'
+import { DatabaseMemory } from './database-memory.js'
 
 const server = fastify()
 
+const database = new DatabaseMemory()
 
+server.post('/videos', (request, reply) => {
+    const {title, description, duration} = request.body
+    
+    database.create({
+        title,
+        description,
+        duration,
+    })
 
-server.post('/videos', () => {
-    return 'Hello World'
+    return reply.status(201).send()
 })
 
-server.get('/videos', () => {
-    return 'Hello helloWorld'
+server.get('/videos', (request, reply) => {
+    const videos = database.list()
+
+
+
+    return videos
 })
 
 server.put('/videos/:id', () => {
