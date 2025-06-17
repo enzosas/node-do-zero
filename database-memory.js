f69750435fabd989d3c0a580295b7a8eb2eaa4f8
Enzo <enzo.silveira2@gmail.com>
2025-06-17 13:04:37 -0300

    put 47:52

--- database-memory.js
+++ database-memory.js
@@ -4,7 +4,16 @@ export class DatabaseMemory {
     #videos = new Map()
 
     list() {
-        return Array.from(this.#videos.values())
+        return Array.from(this.#videos.entries()).map((videoArray) => {
+            const id = videoArray[0]
+            const data = videoArray[1]
+
+            return {
+                id,
+                ...data,
+            }
+        })
+        
     }
     
     create(video) {
@@ -14,7 +23,7 @@ export class DatabaseMemory {
     }
 
     update(id, video) {
-        this.#videos.push(id, video)
+        this.#videos.set(id, video)
     }
 
     delete(id) {
--- server.js
+++ server.js
@@ -35,8 +35,17 @@ server.get('/videos', (request, reply) => {
     return videos
 })
 
-server.put('/videos/:id', () => {
-    return 'Hello helloWorld'
+server.put('/videos/:id', (request, reply) => {
+    const videoId = request.params.id
+    const {title, description, duration} = request.body
+    
+    database.update(videoId, {
+        title,
+        description,
+        duration,
+    })
+
+    return reply.status(204).send()
 })
 
 server.delete('/videos/:id', () => {
