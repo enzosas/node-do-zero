b0f750435e21ef30e0a8312f5610de2fe5d591f0
Enzo <enzo.silveira2@gmail.com>
2025-06-17 08:14:39 -0300

    minor changes

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
-import { createServer } from 'node:http'
+// import { createServer } from 'node:http'
 
-const server = createServer((request, response) => {
-    response.write("oi")
+// const server = createServer((request, response) => {
+//     response.write("oi")
     
-    return response.end()
+//     return response.end()
+// })
+
+// server.listen(3333)
+
+import { fastify } from 'fastify'
+
+const server = fastify()
+
+server.get('/', () => {
+    return 'Hello World'
 })
 
-server.listen(3333)
+server.get('/hello', () => {
+    return 'Hello helloWorld'
+})
+
+server.listen({
+    port: 3333,
+})
